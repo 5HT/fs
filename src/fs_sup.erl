82eaf937f8ca8b2eed0dd0c12aabca655adb4834
Namdak Tonpa <maxim@synrc.com>
2015-03-08 14:01:59 +0200

    close #5

--- src/fs_sup.erl
+++ src/fs_sup.erl
@@ -5,17 +5,16 @@
 -define(CHILD(I, Type, Args), {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
 
 start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
-
 init([]) ->
     Backend = case os:type() of
         {unix, darwin} -> fsevents;
         {unix, linux} -> inotifywait;
         {win32, nt} -> inotifywait_win32;
         _ -> throw(os_not_supported) end,
-    case Backend:find_executable() of
-        false -> io:format("Backend port not found: ~p~n\r",[Backend]);
-        _ -> ok end,
-    Path = fs:path(),
-    {ok, { {one_for_one, 5, 10}, [
-                ?CHILD(fs_server, worker, [Backend, Path, Path]),
-                ?CHILD(gen_event, worker, [{local, fs_events}]) ] } }.
+
+    Children = case Backend:find_executable() of
+        false -> error_logger:error_msg("Backend port not found: ~p~n",[Backend]), [];
+        _ -> Path = fs:path(), [?CHILD(fs_server, worker, [Backend, Path, Path])] end,
+
+    {ok, { {one_for_one, 5, 10},
+        Children ++ [?CHILD(gen_event, worker, [{local, fs_events}])]} }.
