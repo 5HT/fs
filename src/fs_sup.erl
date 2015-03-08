3a0efdb302a1f7c2ce405781522b9d3a7de6ebd2
José Valim <jose.valim@plataformatec.com.br>
2015-03-08 14:11:16 +0100

    Improvements to Mac and non-supported OS

--- src/fs_sup.erl
+++ src/fs_sup.erl
@@ -10,11 +10,24 @@ init([]) ->
         {unix, darwin} -> fsevents;
         {unix, linux} -> inotifywait;
         {win32, nt} -> inotifywait_win32;
-        _ -> throw(os_not_supported) end,
+        _ -> undefined end,
 
-    Children = case Backend:find_executable() of
-        false -> error_logger:error_msg("Backend port not found: ~p~n",[Backend]), [];
-        _ -> Path = fs:path(), [?CHILD(fs_server, worker, [Backend, Path, Path])] end,
+    Children = case has_executable(Backend) of
+        false -> [];
+        true  -> Path = fs:path(), [?CHILD(fs_server, worker, [Backend, Path, Path])] end,
 
     {ok, { {one_for_one, 5, 10},
         Children ++ [?CHILD(gen_event, worker, [{local, fs_events}])]} }.
+
+has_executable(undefined) ->
+    os_not_supported(), false;
+has_executable(Backend) ->
+    case Backend:find_executable() of
+        false -> backend_port_not_found(Backend), false;
+        _     -> true end.
+
+os_not_supported() ->
+    error_logger:error_msg("fs does not support the current operating system~n",[]).
+
+backend_port_not_found(Backend) ->
+    error_logger:error_msg("backend port not found: ~p~n",[Backend]).
