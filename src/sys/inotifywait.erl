2a365b475c06b09128f79f257a0b8fcb116cf381
José Valim <jose.valim@plataformatec.com.br>
2015-03-08 11:39:05 +0100

    Remove the need for sh dep

--- src/sys/inotifywait.erl
+++ src/sys/inotifywait.erl
@@ -7,8 +7,9 @@ known_events() -> [renamed, closed, modified, isdir, undefined].
 
 start_port(Path, Cwd) ->
     Path1 = filename:absname(Path),
-    Args = [find_executable(), "-m", "-e", "close_write", "-e", "moved_to", "-e", "create", "-r", Path1],
-    erlang:open_port({spawn_executable, sh:fdlink_executable()},
+    Args = ["-c", "inotifywait $0 $@ & PID=$!; read a; kill $PID",
+            "-m", "-e", "close_write", "-e", "moved_to", "-e", "create", "-r", Path1],
+    erlang:open_port({spawn_executable, os:find_executable("sh")},
         [stream, exit_status, {line, 16384}, {args, Args}, {cd, Cwd}]).
 
 line_to_event(Line) ->
