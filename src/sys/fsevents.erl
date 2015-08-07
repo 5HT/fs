3ed6b69ffa0d4145cc2af8a8d8e87c777dd7f286
Daniel (dmilith) Dettlaff <dmilith@me.com>
2015-08-07 12:56:21 +0200

    Set abs path to fsevents helper from config

--- src/sys/fsevents.erl
+++ src/sys/fsevents.erl
@@ -3,8 +3,8 @@
 -export(?API).
 
 find_executable() ->
-    case application:get_env(fs, project_dir) of
-        {ok, P} -> fs:find_executable("mac_listener", lists:append([P, "deps/fs/priv/mac_listener"]));
+    case application:get_env(fs, events_helper) of
+        {ok, P} -> fs:find_executable("mac_listener", P);
         undefined -> fs:find_executable("mac_listener", "deps/fs/priv/mac_listener") end.
 
 known_events() ->
