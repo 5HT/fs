df3f6ec724978cde373c47e7eeb7836871e3e32e
Dmitry Russ <dmitry.a.russ@gmail.com>
2016-01-01 19:18:43 +0100

    add backwards compatibility to API, which is enabled per default

--- src/fs_app.erl
+++ src/fs_app.erl
@@ -2,5 +2,8 @@
 -behaviour(application).
 -export([start/2, stop/1]).
 
-start(_StartType, _StartArgs) -> {ok, self()}.
+start(_StartType, _StartArgs) ->
+    case application:get_env(fs, backwards_compatible) of
+        {ok, false} -> {ok, self()};
+        {ok, true} -> fs:start_link(default_fs) end.
 stop(_State) -> ok.
