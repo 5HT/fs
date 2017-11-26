c4f07963ffdd7ce1064671ae2e04fdd072f14929
Holger Weiss <holger@zedat.fu-berlin.de>
2017-11-26 01:03:35 +0100

    Fix open_port/2 call for kqueue backend
    The port command arguments must be specified as a list.

--- src/sys/kqueue.erl
+++ src/sys/kqueue.erl
@@ -9,4 +9,4 @@ line_to_event(Line) ->
 find_executable() -> fs:find_executable("kqueue", "deps/fs/priv/kqueue").
 start_port(Path, Cwd) ->
     erlang:open_port({spawn_executable, find_executable()},
-        [stream, exit_status, {line, 16384}, {args, Path}, {cd, Cwd}]).
+        [stream, exit_status, {line, 16384}, {args, [Path]}, {cd, Cwd}]).
