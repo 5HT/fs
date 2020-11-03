f70dce4b4bc5800916f9e8924391245eda715380
Dominic Letz <dominic@diode.io>
2020-11-03 16:08:38 +0100

    Fix watching directories with spaces on Linux

--- src/sys/inotifywait.erl
+++ src/sys/inotifywait.erl
@@ -7,7 +7,7 @@ known_events() -> [created, deleted, renamed, closed, modified, isdir, attribute
 
 start_port(Path, Cwd) ->
     Path1 = filename:absname(Path),
-    Args = ["-c", "inotifywait $0 $@ & PID=$!; read a; kill $PID",
+    Args = ["-c", "inotifywait \"$0\" \"$@\" & PID=$!; read a; kill $PID",
             "-m", "-e", "modify", "-e", "close_write", "-e", "moved_to", "-e", "create", "-e", "delete",
             "-e", "attrib", "--quiet", "-r", Path1],
     erlang:open_port({spawn_executable, os:find_executable("sh")},
