a88ae82b1062db1a26b24e7d30b413b365a37d9e
5HT <maxim@synrc.com>
2022-06-17 17:40:02 +0300

    synrc/fs#69 manual merge

--- src/fs_server.erl
+++ src/fs_server.erl
@@ -13,7 +13,7 @@
          terminate/2,
          code_change/3]).
 
--record(state, {event_handler, port, path, backend}).
+-record(state, {event_handler, port, path, backend, cwd, crashes}).
 
 notify(EventHandler, file_event = A, Msg) ->
     Key = {fs, A},
@@ -29,7 +29,7 @@ init([EventHandler, Backend, Path, Cwd]) ->
     {ok,
      #state{event_handler = EventHandler,
             port = Backend:start_port(Path, Cwd), path = Path,
-            backend = Backend}}.
+            backend = Backend,cwd=Cwd,crashes=0}}.
 
 handle_call(known_events, _From,
             #state{backend = Backend} = State) ->
@@ -50,8 +50,12 @@ handle_info({_Port, {data, {noeol, Line}}}, State) ->
     error_logger:error_msg("~p line too long: ~p, ignoring~n",
                            [?SERVER, Line]),
     {noreply, State};
-handle_info({_Port, {exit_status, Status}}, State) ->
-    {stop, {port_exit, Status}, State};
+
+handle_info({_Port, {exit_status, Status}}, #state{path=Path,backend=Backend,cwd=Cwd,crashes=Crashes} = State) ->
+    error_logger:error_msg("~p port_exit ~p, retry ~p~n", [?SERVER, Status, Crashes]),
+    timer:sleep(100*(Crashes+1)*(Crashes+1)),
+    {noreply, State#state{port=Backend:start_port(Path, Cwd),crashes=Crashes+1}};
+
 handle_info(_Info, State) -> {noreply, State}.
 
 terminate(_Reason, #state{port = Port}) ->
